f5dca93eb6209f53e2ea8cd848649568191040e3
Rok Klancar <rklancar@gmail.com>
2020-04-09 11:38:27 +0200

    JavaScript code now only allows selecting cards in the same row

--- user_web_interface/static/app.js
+++ user_web_interface/static/app.js
@@ -1,3 +1,5 @@
+// Show/Hide game rules
+
 let instructionsText = document.getElementById('id_game_rules');
 let instructionsLink = document.getElementById('id_instructions_link');
 
@@ -13,32 +15,23 @@ function showInstructions() {
     }
 }
 
+// Select only cards from the same row
 
-var rows = document.getElementsByClassName('row');
-console.log(rows)
+let allCards = document.querySelectorAll('label[for^="id_row"]');
 
-function getAllRowsButThisOne(clickedRow) {
-    for (let row of rows) {
-        console.log(clickedRow)
-        if (row == clickedRow) {
-        } else {
-            var allInputsInRow = row.getElementsByTagName('input');
-            for (let input of allInputsInRow) {
-                input.checked = false;
-            }
-        }
-    }
+for (let index = 0, len = allCards.length; index < len; index++) {
+	allCards[index].addEventListener('click', uncheckCardsInOtherRows);
+};
+
+function uncheckCardsInOtherRows (e) {
+	let currentRow = e.target.getAttribute('for').slice(3, 7);
+	for (let index = 0, len = allCards.length; index < len; index++) {
+		if (allCards[index].getAttribute('for').slice(3, 7) == currentRow) {
+			
+		} else {
+			var inputId = allCards[index].getAttribute('for');
+			document.getElementById(inputId).checked = false;
+		}
+	}
 }
-        
-/*for (let row of rows) {
-    row.addEventListener('click', function () {
 
-        getAllRowsButThisOne(row);
-    });
-}
-*/
-function uncheckBoxesInOtherRows() {
-    console.log(pa.id);
-    // var allRowsButThisOne = rows - 
-
-}
