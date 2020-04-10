3f3904b7a858bcfda4a2968fbef2df11f703718d
Rok Klancar <rklancar@gmail.com>
2020-04-10 12:31:46 +0200

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- WebSticker/settings.py
+++ WebSticker/settings.py
@@ -19,6 +19,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'insecure-key-for-dev'
+    ALLOWED_HOSTS = []
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^vx*r5*)z$7+j)1+a)njl8&be_s1715)6%gueiq7fx!=)iztyo'
 
